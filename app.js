8ecc27d5a4cf083b97c292f003401224e1f62cb0
Andrii-Hr <andriihryb@ukr.net>
2024-03-11 17:07:43 +0200

    aa

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const app = express();
 app.use(morgan("tiny"));
 app.use(cors());
 app.use(express.json());
+app.use(express.static("public"));
 
 app.use("/api/auth", authRouter);
 app.use("/api/contacts", contactsRouter);
@@ -38,6 +39,3 @@ mongoose
     console.log(err.message);
     process.exit(1);
   });
-
-  {"email":"testemail.com",
-  password:"123456"}
